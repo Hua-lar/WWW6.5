0d930dba0e9be1abf14a679480b1d0beb46caadb
Hua <wangzihua5107@gmail.com>
2026-03-21 23:09:49 +0800

    Update day17-SubscriptionLogicV1.sol

--- Hua/day17-SubscriptionLogicV1.sol
+++ Hua/day17-SubscriptionLogicV1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./day17_SubscriptionStorageLayout.sol";
+import "./day17-SubscriptionStorageLayout.sol";
 
 contract SubscriptionLogicV1 is SubscriptionStorageLayout {
     function addPlan(uint8 planId, uint256 price, uint256 duration) external {
